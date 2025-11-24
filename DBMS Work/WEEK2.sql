4db7bf78d3535d0c6d2ce8a103d793f812af24f8
Mohammad Iqbal Hurrah <mohammadiqbalhurrah@gmail.com>
2025-11-24 20:22:48 +0530

    DSA using C++

--- DBMS Work/WEEK2.sql
+++
@@ -1,7 +0,0 @@
---Create a students table with id, name,age, marks. Add a new column grade. Drop a temporary--
-use Student_Db;
-CREATE TABLE students 
-(id INT, name VARCHAR(20), age INT, marks INT);
-SELECT * FROM students;
-ALTER TABLE students ADD grade INT;
-DROP TABLE students;
